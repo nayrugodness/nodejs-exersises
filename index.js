dabadabd88c26db9c88f3bdafad91e89525cfe03
Nayru <nanualexandraramirez@gmail.com>
2022-11-11 06:50:40 -0500

    copying the exact example code that we are going to change right now

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express()
+const port = 10101;
+
+app.get('/json', (req, res) => {
+    res.status(200).json({status: 'Todo ok'});
+  })
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
